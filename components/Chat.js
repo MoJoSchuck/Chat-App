e513150b4f6bd57a4882674f6b8f1660dbf6199f
Moritz Schuck <159003684+MoJoSchuck@users.noreply.github.com>
2024-07-18 16:12:44 +0200

    Update Chat.js

--- components/Chat.js
+++ components/Chat.js
@@ -1,15 +1,41 @@
 import { useEffect, useState } from 'react';
-import { StyleSheet, View, Text, Platform, KeyboardAvoidingView } from 'react-native';
+import { StyleSheet, View, Platform, KeyboardAvoidingView } from 'react-native';
 import { GiftedChat, Bubble } from 'react-native-gifted-chat';
+import { collection, addDoc, onSnapshot, query, orderBy } from "firebase/firestore";
 
-const Chat = ({ route, navigation }) => {
-    const { name, color } = route.params;
+const Chat = ({ db, route, navigation }) => {
+    const { name, selectedColor, userID } = route.params;
     const [messages, setMessages] = useState([]);
+    
+    useEffect(() => {
+        navigation.setOptions({ title: name });
+        const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
+        const unsubMessages = onSnapshot(q, (docs) => {
+            let newMessages = [];
+            docs.forEach(doc => {
+                newMessages.push({
+                    id: doc.id,
+                    ...doc.data(),
+                    createdAt: new Date(doc.data().createdAt.toMillis())
+                });
+            });
+            setMessages(newMessages);
+        });
+   
+        // Clean up code
+        return () => {
+            if (unsubMessages) unsubMessages();
+        }
+    }, []);
+
+    useEffect(() => {
+        navigation.setOptions({ title: name });
+    }, [name, navigation]);
+
     const onSend = (newMessages) => {
-        setMessages((previousMessages) =>
-            GiftedChat.append(previousMessages, newMessages)
-        );
-    };
+        addDoc(collection(db, "messages"), newMessages[0]);
+    }
+
     const renderBubble = (props) => {
         return <Bubble
             {...props}
@@ -24,39 +50,15 @@ const Chat = ({ route, navigation }) => {
         />
     }
 
-    useEffect(() => {
-        setMessages([
-            {
-                _id: 1,
-                text: 'Hello developer',
-                createdAt: new Date(),
-                user: {
-                    _id: 2,
-                    name: 'React Native',
-                    avatar: 'https://placeimg.com/140/140/any',
-                },
-            },
-            {
-                _id: 2,
-                text: 'This is a system message',
-                createdAt: new Date(),
-                system: true,
-            },
-        ]);
-    }, []);
-
-    useEffect(() => {
-        navigation.setOptions({ title: name });
-    }, [name, navigation]);
-
     return (
-        <View style={[styles.container, { backgroundColor: color }]}>
+        <View style={[styles.container, { backgroundColor: selectedColor }]}>
             <GiftedChat
                 messages={messages}
                 renderBubble={renderBubble}
                 onSend={(messages) => onSend(messages)}
                 user={{
-                    _id: 1,
+                    _id: userID,
+                    name: name,
                 }}
             />
             {Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null}
