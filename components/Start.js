3460b8416c21cac6729fc22f13290a2ac7b8ba85
Moritz Schuck <159003684+MoJoSchuck@users.noreply.github.com>
2024-07-19 09:02:43 +0200

    Update Start.js

--- components/Start.js
+++ components/Start.js
@@ -66,7 +66,7 @@ const styles = StyleSheet.create({
         height: "44%",
         backgroundColor: "white",
         alignItems: "center",
-        marginBottom: 20,
+        marginBottom: 90,
         justifyContent: "space-evenly",
         borderRadius: 4,
     },
