a45018ded0e817c30ae093a0e4e9a2ede5cb96cc
Moritz Schuck <159003684+MoJoSchuck@users.noreply.github.com>
2024-07-23 10:05:59 +0200

    Update Chat.js

--- components/Chat.js
+++ components/Chat.js
@@ -1,37 +1,12 @@
 import { useEffect, useState } from 'react';
 import { StyleSheet, View, Platform, KeyboardAvoidingView } from 'react-native';
-import { GiftedChat, Bubble } from 'react-native-gifted-chat';
+import { GiftedChat, Bubble, InputToolbar } from 'react-native-gifted-chat';
 import { collection, addDoc, onSnapshot, query, orderBy } from "firebase/firestore";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const Chat = ({ db, route, navigation }) => {
+const Chat = ({ db, route, navigation, isConnected }) => {
     const { name, selectedColor, userID } = route.params;
     const [messages, setMessages] = useState([]);
-    
-    useEffect(() => {
-        navigation.setOptions({ title: name });
-        const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
-        const unsubMessages = onSnapshot(q, (docs) => {
-            let newMessages = [];
-            docs.forEach(doc => {
-                newMessages.push({
-                    id: doc.id,
-                    ...doc.data(),
-                    createdAt: new Date(doc.data().createdAt.toMillis())
-                });
-            });
-            setMessages(newMessages);
-        });
-   
-        // Clean up code
-        return () => {
-            if (unsubMessages) unsubMessages();
-        }
-    }, []);
-
-    useEffect(() => {
-        navigation.setOptions({ title: name });
-    }, [name, navigation]);
-
     const onSend = (newMessages) => {
         addDoc(collection(db, "messages"), newMessages[0]);
     }
@@ -50,11 +25,69 @@ const Chat = ({ db, route, navigation }) => {
         />
     }
 
+    const renderInputToolbar = (props) => {
+        if (isConnected) return <InputToolbar {...props} />;
+        else return null;
+    }
+
+    // useEffect hook to set messages options
+    let unsubMessages
+    useEffect(() => {
+
+        if (isConnected === true) {
+
+            // unregister current onSnapshot() listener to avoid registering multiple listeners when
+            // useEffect code is re-executed.
+            if (unsubMessages) unsubMessages();
+            unsubMessages = null;
+            navigation.setOptions({ title: name });
+
+            // Create a query to get the "messages" collection from the Firestore database
+            const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
+            // This function will be called whenever there are changes in the collection
+            unsubMessages = onSnapshot(q, (docs) => {
+                let newMessages = [];
+                // Iterate through each document in the snapshot
+                docs.forEach(doc => {
+                    newMessages.push({
+                        id: doc.id,
+                        ...doc.data(),
+                        createdAt: new Date(doc.data().createdAt.toMillis())
+                    });
+                });
+                cacheMessages(newMessages);
+                setMessages(newMessages);
+            });
+        } else loadCachedMessages();
+
+
+        // Clean up code
+        return () => {
+            if (unsubMessages) unsubMessages();
+        }
+    }, [isConnected]); //isConnected used as a dependency value enabling the component to call the callback of useEffect whenewer the isConnected prop's value changes
+
+    const cacheMessages = async (messagesToCache) => {
+        try {
+            await AsyncStorage.setItem("messages", JSON.stringify(messagesToCache));
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
+
+    // Call this function when isConnected prop turns out to be false in useEffect()
+    const loadCachedMessages = async () => {
+        // The empty array is for cachedMessages in case AsyncStorage() fails when the messages item hasn’t been set yet in AsyncStorage.
+        const cachedMessages = (await AsyncStorage.getItem("messages")) || [];
+        setMessages(JSON.parse(cachedMessages));
+    };
+
     return (
         <View style={[styles.container, { backgroundColor: selectedColor }]}>
             <GiftedChat
                 messages={messages}
                 renderBubble={renderBubble}
+                renderInputToolbar={renderInputToolbar}
                 onSend={(messages) => onSend(messages)}
                 user={{
                     _id: userID,
