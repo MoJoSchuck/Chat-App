8312cefff9de24fcab5f0cd290cb03ae03ffae45
Moritz Schuck <159003684+MoJoSchuck@users.noreply.github.com>
2024-07-25 09:42:06 +0200

    Update Chat.js

--- components/Chat.js
+++ components/Chat.js
@@ -1,16 +1,25 @@
+// Import necessary libraries and components
 import { useEffect, useState } from 'react';
 import { StyleSheet, View, Platform, KeyboardAvoidingView } from 'react-native';
 import { GiftedChat, Bubble, InputToolbar } from 'react-native-gifted-chat';
 import { collection, addDoc, onSnapshot, query, orderBy } from "firebase/firestore";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import CustomActions from './CustomActions';
+import MapView from 'react-native-maps';
 
-const Chat = ({ db, route, navigation, isConnected }) => {
+const Chat = ({ db, route, navigation, isConnected, storage }) => {
+    // Destructure parameters received from route
     const { name, selectedColor, userID } = route.params;
+
+    // State to hold chat messages
     const [messages, setMessages] = useState([]);
+
+    // Function to send new messages
     const onSend = (newMessages) => {
         addDoc(collection(db, "messages"), newMessages[0]);
     }
 
+    // Custom bubble rendering for messages
     const renderBubble = (props) => {
         return <Bubble
             {...props}
@@ -25,26 +34,22 @@ const Chat = ({ db, route, navigation, isConnected }) => {
         />
     }
 
+    // Custom input toolbar rendering based on connection status
     const renderInputToolbar = (props) => {
         if (isConnected) return <InputToolbar {...props} />;
         else return null;
     }
 
-    // useEffect hook to set messages options
     let unsubMessages
     useEffect(() => {
-
         if (isConnected === true) {
-
-            // unregister current onSnapshot() listener to avoid registering multiple listeners when
-            // useEffect code is re-executed.
+            // Unregister current onSnapshot() listener to avoid registering multiple listeners
             if (unsubMessages) unsubMessages();
             unsubMessages = null;
             navigation.setOptions({ title: name });
 
             // Create a query to get the "messages" collection from the Firestore database
             const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
-            // This function will be called whenever there are changes in the collection
             unsubMessages = onSnapshot(q, (docs) => {
                 let newMessages = [];
                 // Iterate through each document in the snapshot
@@ -61,12 +66,13 @@ const Chat = ({ db, route, navigation, isConnected }) => {
         } else loadCachedMessages();
 
 
-        // Clean up code
+        // Clean up code to unregister onSnapshot() listener
         return () => {
             if (unsubMessages) unsubMessages();
         }
     }, [isConnected]); //isConnected used as a dependency value enabling the component to call the callback of useEffect whenewer the isConnected prop's value changes
 
+    // Function to cache messages to AsyncStorage
     const cacheMessages = async (messagesToCache) => {
         try {
             await AsyncStorage.setItem("messages", JSON.stringify(messagesToCache));
@@ -75,12 +81,41 @@ const Chat = ({ db, route, navigation, isConnected }) => {
         }
     };
 
-    // Call this function when isConnected prop turns out to be false in useEffect()
+    // Function to load cached messages from AsyncStorage when offline
     const loadCachedMessages = async () => {
-        // The empty array is for cachedMessages in case AsyncStorage() fails when the messages item hasn’t been set yet in AsyncStorage.
+        /// The empty array is for cachedMessages in case AsyncStorage fails when the messages item hasn’t been set yet
         const cachedMessages = (await AsyncStorage.getItem("messages")) || [];
         setMessages(JSON.parse(cachedMessages));
     };
+
+    // Renders custom actions menu for the chat
+    const renderCustomActions = (props) => {
+        return <CustomActions storage={storage} {...props} />;
+    };
+
+    // Renders custom view for map location messages
+    const renderCustomView = (props) => {
+        const { currentMessage } = props;
+        if (currentMessage.location) {
+            return (
+                <MapView
+                    style={{
+                        width: 150,
+                        height: 100,
+                        borderRadius: 13,
+                        margin: 3
+                    }}
+                    region={{
+                        latitude: currentMessage.location.latitude,
+                        longitude: currentMessage.location.longitude,
+                        latitudeDelta: 0.0922,
+                        longitudeDelta: 0.0421,
+                    }}
+                />
+            );
+        }
+        return null;
+    }
 
     return (
         <View style={[styles.container, { backgroundColor: selectedColor }]}>
@@ -89,6 +124,8 @@ const Chat = ({ db, route, navigation, isConnected }) => {
                 renderBubble={renderBubble}
                 renderInputToolbar={renderInputToolbar}
                 onSend={(messages) => onSend(messages)}
+                renderActions={renderCustomActions}
+                renderCustomView={renderCustomView}
                 user={{
                     _id: userID,
                     name: name,
